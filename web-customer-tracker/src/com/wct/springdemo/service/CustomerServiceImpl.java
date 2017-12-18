f1a6815f106f1842235e17e762bd32e5be89b7ef
AmerSarajlic <amer_sarajlic@hotmail.com>
2017-12-18 18:40:21 +0100

    Refactor: Add a Service Layer

---
+++ web-customer-tracker/src/com/wct/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.wct.springdemo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wct.springdemo.dao.CustomerDAO;
+import com.wct.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO customerDao;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDao.getCustomers();
+	}
+}
